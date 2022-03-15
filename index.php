0dca296547f214ef7a20ef8b8a0aae3a332d0a96
nmathewsPVA <54141301+nmathewsPVA@users.noreply.github.com>
2022-03-15 12:14:57 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,45 @@
 <?php
 // PVA's internal Vehicle Status Board
+$url = "https://www.monroecounty.gov/etc/ambulance/json.php?u=PITE&p=s24.PITE.42";
 
-// $url = "https://www.monroecounty.gov/etc/ambulance/json.php?u=PITE&p=s24.PITE.42";
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-// $curl = curl_init($url);
-// curl_setopt($curl, CURLOPT_URL, $url);
-// curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-// $headers = array(
-//    "Accept: application/json",
-//    "Access-Control-Allow-Origin: *",
-// );
-// curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+$headers = array(
+   "Accept: application/json",
+   "Access-Control-Allow-Origin: *",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 //for debug only!
-// curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-// curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
-// $resp = curl_exec($curl);
-// curl_close($curl);
-// $json = json_decode($resp, true);
+$json = curl_exec($curl);
+// debug
+if(curl_exec($curl) === false)
+{
+    echo 'Curl error: ' . curl_error($ch);
+}
+else
+{
+    echo 'Operation completed without any errors';
+}
 
-// Read the JSON file
-$json = file_get_contents('data.json');
+// close curl
+curl_close($curl);
 
-// Decode the JSON file
+// decode json data into usable object
 $json_data = json_decode($json,true);
 
+// Read the JSON file
+//$json = file_get_contents('data.json');
+
+// Decode the JSON file
+//$json_data = json_decode($json,true);
+
 // Print the JSON data
-// print_r($json_data['currentdata']);
+print_r($json_data);
 
 ?>
 <!doctype html>
@@ -52,57 +64,77 @@ $json_data = json_decode($json,true);
 			background-color: #33CC00 !important;
 			color: #ffffff;
 		}
+		.bucket {
+			border: 1px solid rgba(0,0,0,.125);
+		}
 		h2.duty-crew-heading, h2.on-call-heading, h2.on-duty-heading {
 			text-align: center;
 		}
+		.card {
+			border: none;
+		}
+		.vehicle-card > .card > span.fa-solid {
+			font-size: 2rem;
+		}
 	</style>
     <title>PVA Status Board</title>
+	<link href="assets/fontawesome-free-6.0.0-web/css/all.css" rel="stylesheet" >
   </head>
   <body>
 	<h1 class="text-center">PVA Status Board</h1>
 	<div class="supervisors container">
 		<div class="row">
 			<div class="col">
-				<h2 class="on-call-heading">On-Call Chief</h2>
-				<table class="table">
-					<?php
-						// iterate through JSON currentdata and populate crew data for On-Call Chief
-						$search_val_chief = '2181';
-						foreach($json_data as $elem) {
-							foreach($elem['currentdata'] as $key => $val) {
-								if ($key == 'vehicle_id' && val == $search_val_chief) {
-									echo("<tr><td>".$elem['first_name']." ".$elem['last_name']."</td><td>".$elem['phone']."</td><td>".$elem['in']."</td><td>".$elem['out']."</td></tr>");
-								} else {
-									echo("<tr><td><i>none listed</i></td></tr>");
+					<div class="container-fluid bucket">
+					<h2 class="on-call-heading">On-Call Chief</h2>
+					<table class="table">
+						<?php
+							// iterate through JSON currentdata and populate crew data for On-Call Chief
+							$search_val_chief = '2181';
+							if($json_data) {
+								foreach($json_data['currentdata'] as $currentdata) {
+									if ($currentdata['vehicle_id'] == $search_val_chief) {
+										echo("<tr><td>".$currentdata['first_name']." ".$currentdata['last_name']."</td><td>".$currentdata['phone']."</td><td>".date('n/j  H:i', strtotime($currentdata['in']))."</td><td>".date('n/j  H:i', strtotime($currentdata['out']))."</td></tr>");
+									}
 								}
+							} else {
+								echo("<tr><td><i>none statused</i></td></tr>");
 							}
-						}
-					?>
-				</table>
+						?>
+					</table>
+				</div>
 			</div>
 			<div class="col">
-				<h2 class="on-duty-heading">Shift Supervisor</h2>
-				<table class="table">
-					<?php
-						// iterate through JSON currentdata and populate crew data for MED36
-						$search_val_supv = '1450';
-					?>
-					<tr>
-						<td>Jenny Carver - 3M58</td>
-						<td>585-385-2401</td>
-						<td>0600</td>
-						<td>1800</td>
-					</tr>
-				</table>
+				<div class="container-fluid bucket">
+					<h2 class="on-duty-heading">Shift Supervisor</h2>
+					<table class="table">
+						<?php
+							// iterate through JSON currentdata and populate crew data for MED36
+							$search_val_supv = '1450';
+							if($json_data) {
+								foreach($json_data['currentdata'] as $currentdata) {
+									if ($currentdata['vehicle_id'] == $search_val_supv) {
+										echo("<tr><td>".$currentdata['first_name']." ".$currentdata['last_name']."</td><td>".$currentdata['phone']."</td><td>".date('n/j  H:i', strtotime($currentdata['in']))."</td><td>".date('n/j  H:i', strtotime($currentdata['out']))."</td></tr>");
+									}
+								}
+							} else {
+								echo("<tr><td><i>none statused</i></td></tr>");
+							}
+						?>
+					</table>
+				</div>
 			</div>
 		</div>
 	</div>
 	<h2 class="duty-crew-heading">On-Duty Crews</h2>
 	<div class="vehicles container">
 		<div id="3859" class="vehicle row">
-			<div class="vehicle-card col-1 position-relative">
-				<div class="position-absolute top-50 start-50 translate-middle">
-					3859
+			<div class="vehicle-card col-1">
+				<div class="card text-center">
+					<span class="fa-solid fa-truck-medical"></span>
+				</div>
+				<div class="card-body">
+					<h5 class="card-title text-center">3859</h5>
 				</div>
 			</div>
 			<div class="crew-table-wrapper col-11">
@@ -110,24 +142,26 @@ $json_data = json_decode($json,true);
 					<?php
 						// iterate through JSON currentdata and populate crew data for 3859
 						$search_val_3859 = '1990';
+						if($json_data) {
+							foreach($json_data['currentdata'] as $currentdata) {
+								if ($currentdata['vehicle_id'] == $search_val_3859) {
+									echo("<tr><td class=\"".$currentdata['level']."\">".$currentdata['level']."</td><td>".$currentdata['first_name']." ".$currentdata['last_name']."</td><td>".$currentdata['phone']."</td><td>".date('n/j  H:i', strtotime($currentdata['in']))."</td><td>".date('n/j  H:i', strtotime($currentdata['out']))."</td><td>".$currentdata['notes']."</tr>");
+								}
+							}
+						} else {
+							echo("<tr><td><i>no crew statused</i></td></tr>");
+						}
 					?>
-					<tr>
-						<td class="text-center">ALS</td>
-						<td>Neil Mathews</td>
-						<td>585-747-3255</td>
-					</tr>
-					<tr>
-						<td class="BLS text-center">BLS</td>
-						<td>Jenny Carver</td>
-						<td>585-385.2401</td>
-					</tr>
 				</table>
 			</div>
 		</div>
 		<div id="3869" class="vehicle row">
-			<div class="vehicle-card col-1 position-relative">
-				<div class="position-absolute top-50 start-50 translate-middle">
-					3869
+			<div class="vehicle-card col-1">
+				<div class="card text-center">
+					<span class="fa-solid fa-truck-medical"></span>
+				</div>
+				<div class="card-body">
+					<h5 class="card-title text-center">3869</h5>
 				</div>
 			</div>
 			<div class="crew-table-wrapper col-11">
@@ -135,24 +169,26 @@ $json_data = json_decode($json,true);
 					<?php
 						// iterate through JSON currentdata and populate crew data for 3869
 						$search_val_3869 = '1991';
+						if($json_data) {
+							foreach($json_data['currentdata'] as $currentdata) {
+								if ($currentdata['vehicle_id'] == $search_val_3869) {
+									echo("<tr><td class=\"".$currentdata['level']."\">".$currentdata['level']."</td><td>".$currentdata['first_name']." ".$currentdata['last_name']."</td><td>".$currentdata['phone']."</td><td>".date('n/j  H:i', strtotime($currentdata['in']))."</td><td>".date('n/j  H:i', strtotime($currentdata['out']))."</td></tr>");
+								}
+							}
+						} else {
+							echo("<tr><td><i>no crew statused</i></td></tr>");
+						}
 					?>
-					<tr>
-						<td>ALS</td>
-						<td>Neil Mathews</td>
-						<td>585-747-3255</td>
-					</tr>
-					<tr>
-						<td>BLS</td>
-						<td>Jenny Carver</td>
-						<td>585-385.2401</td>
-					</tr>
 				</table>
 			</div>
 		</div>
 		<div id="3879" class="vehicle row">
-			<div class="vehicle-card col-1 position-relative">
-				<div class="position-absolute top-50 start-50 translate-middle">
-					3879
+			<div class="vehicle-card col-1">
+				<div class="card text-center">
+					<span class="fa-solid fa-truck-medical"></span>
+				</div>
+				<div class="card-body">
+					<h5 class="card-title text-center">3879</h5>
 				</div>
 			</div>
 			<div class="crew-table-wrapper col-11">
@@ -160,44 +196,53 @@ $json_data = json_decode($json,true);
 					<?php
 						// iterate through JSON currentdata and populate crew data for 3879
 						$search_val_3879 = '1992';
+						if($json_data) {
+							foreach($json_data['currentdata'] as $currentdata) {
+								if ($currentdata['vehicle_id'] == $search_val_3879) {
+									echo("<tr><td class=\"".$currentdata['level']."\">".$currentdata['level']."</td><td>".$currentdata['first_name']." ".$currentdata['last_name']."</td><td>".$currentdata['phone']."</td><td>".date('n/j  H:i', strtotime($currentdata['in']))."</td><td>".date('n/j  H:i', strtotime($currentdata['out']))."</td></tr>");
+								}
+							}
+						} else {
+							echo("<tr><td><i>no crew statused</i></td></tr>");
+						}
 					?>
-					<tr>
-						<td>ALS</td>
-						<td>Neil Mathews</td>
-						<td>585-747-3255</td>
-					</tr>
-					<tr>
-						<td>BLS</td>
-						<td>Jenny Carver</td>
-						<td>585-385.2401</td>
-					</tr>
 				</table>
 			</div>
 		</div>
 		<div id="MED36" class="vehicle row">
-			<div class="vehicle-card col-1 position-relative">
-				<div class="position-absolute top-50 start-50 translate-middle">
-					MED36
+			<div class="vehicle-card col-1">
+				<div class="card text-center">
+					<span class="fa-solid fa-car-side"></span>
+				</div>
+				<div class="card-body">
+					<h5 class="card-title text-center">MED36</h5>
 				</div>
 			</div>
 			<div class="crew-table-wrapper col-11">
 				<table class="table crew">
-					<tr>
 					<?php
 						// iterate through JSON currentdata and populate crew data for MED36
 						$search_val_36 = '1551';
+						if($json_data) {
+							foreach($json_data['currentdata'] as $currentdata) {
+								if ($currentdata['vehicle_id'] == $search_val_36) {
+									echo("<tr><td class=\"".$currentdata['level']."\">".$currentdata['level']."</td><td>".$currentdata['first_name']." ".$currentdata['last_name']."</td><td>".$currentdata['phone']."</td><td>".date('n/j  H:i', strtotime($currentdata['in']))."</td><td>".date('n/j  H:i', strtotime($currentdata['out']))."</td></tr>");
+								}
+							}
+						} else {
+							echo("<tr><td><i>no crew statused</i></td></tr>");
+						}
 					?>
-					<td>ALS</td>
-						<td>Neil Mathews</td>
-						<td>585-747-3255</td>
-					</tr>
 				</table>
 			</div>
 		</div>
 		<div id="MED38" class="vehicle row">
-			<div class="vehicle-card col-1 position-relative">
-				<div class="position-absolute top-50 start-50 translate-middle">
-					MED38
+			<div class="vehicle-card col-1">
+				<div class="card text-center">
+					<span class="fa-solid fa-car-side"></span>
+				</div>
+				<div class="card-body">
+					<h5 class="card-title text-center">MED38</h5>
 				</div>
 			</div>
 			<div class="crew-table-wrapper col-11">
@@ -205,12 +250,16 @@ $json_data = json_decode($json,true);
 					<?php
 						// iterate through JSON currentdata and populate crew data for MED38
 						$search_val_38 = '1552';
+						if($json_data) {
+							foreach($json_data['currentdata'] as $currentdata) {
+								if ($currentdata['vehicle_id'] == $search_val_38) {
+									echo("<tr><td class=\"".$currentdata['level']."\">".$currentdata['level']."</td><td>".$currentdata['first_name']." ".$currentdata['last_name']."</td><td>".$currentdata['phone']."</td><td>".date('n/j  H:i', strtotime($currentdata['in']))."</td><td>".date('n/j  H:i', strtotime($currentdata['out']))."</td></tr>");
+								}
+							}
+						} else {
+							echo("<tr><td><i>no crew statused</i></td></tr>");
+						}
 					?>
-					<tr>
-						<td>ALS</td>
-						<td>Neil Mathews</td>
-						<td>585-747-3255</td>
-					</tr>
 				</table>
 			</div>
 		</div>
