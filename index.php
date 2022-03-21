9cb52f6af7b3f9057500ba5a603fb034e1031893
nmathewsPVA <nmathews@pittsfordambulance.org>
2022-03-21 12:34:24 -0400

    Update index.php

--- index.php
+++ index.php
@@ -63,6 +63,9 @@ $json_data = json_decode($json,true);
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<style>
+		.supervisors.container {
+			margin-bottom: 2rem;
+		}
 		td.level {
 			text-align: center;
 			font-weight: 600;
@@ -86,7 +89,6 @@ $json_data = json_decode($json,true);
 			text-align: center;
 		}
 		h2.duty-crew-heading {
-			padding: 2rem 0 0 0;
 		}
 		.vehicle {
 			padding: 10px 0 0 0;
@@ -125,8 +127,8 @@ $json_data = json_decode($json,true);
 							}
 						?>
 					</table>
-				</div>
-			</div>
+				</div> <!-- end .bucket -->
+			</div> <!-- end .col -->
 			<div class="col">
 				<div class="container-fluid bucket">
 					<h2 class="on-duty-heading">Shift Supervisor</h2>
@@ -143,138 +145,152 @@ $json_data = json_decode($json,true);
 							}
 						?>
 					</table>
-				</div>
-			</div>
-		</div>
-	</div>
-	<h2 class="duty-crew-heading">On-Duty Crews</h2>
-	<div class="vehicles container">
-		<div id="3859" class="vehicle row d-flex align-items-center">
-			<div class="vehicle-card col-1">
-				<div class="card text-center">
-					<span class="fa-solid fa-truck-medical"></span>
-				</div>
-				<div class="card-body">
-					<h5 class="card-title text-center">3859</h5>
-				</div>
-			</div>
-			<div class="crew-table-wrapper col-11">
-				<table class="table crew">
-					<?php
-						// iterate through JSON currentdata and populate crew data for 3859
-						$search_val_3859 = '1990';
-						if($json_data) {
-							foreach($json_data['currentdata'] as $currentdata) {
-								if ($currentdata['vehicle_id'] == $search_val_3859 && strtotime($currentdata['in']) < strtotime('+2 hour',time())) {
-									echo("<tr><td class=\"level ".$currentdata['level']."\">".$currentdata['level']."</td><td>".$currentdata['first_name']." ".$currentdata['last_name']."</td><td>".$currentdata['phone']."</td><td>".date('n/j  H:i', strtotime($currentdata['in']))."</td><td>".date('n/j  H:i', strtotime($currentdata['out']))."</td><td>".$currentdata['notes']."</tr>");
-								}
-							}
-						}
-					?>
-				</table>
-			</div>
-		</div>
-		<div id="3869" class="vehicle row d-flex align-items-center" style="background: #efefef;">
-			<div class="vehicle-card col-1">
-				<div class="card text-center">
-					<span class="fa-solid fa-truck-medical"></span>
-				</div>
-				<div class="card-body">
-					<h5 class="card-title text-center">3869</h5>
-				</div>
-			</div>
-			<div class="crew-table-wrapper col-11">
-				<table class="table crew">
-					<?php
-						// iterate through JSON currentdata and populate crew data for 3869
-						$search_val_3869 = '1991';
-						if($json_data) {
-							foreach($json_data['currentdata'] as $currentdata) {
-								if ($currentdata['vehicle_id'] == $search_val_3869 && strtotime($currentdata['in']) < strtotime('+2 hour',time())) {
-									echo("<tr><td class=\"level ".$currentdata['level']."\">".$currentdata['level']."</td><td>".$currentdata['first_name']." ".$currentdata['last_name']."</td><td>".$currentdata['phone']."</td><td>".date('n/j  H:i', strtotime($currentdata['in']))."</td><td>".date('n/j  H:i', strtotime($currentdata['out']))."</td></tr>");
-								}
-							}
-						}
-					?>
-				</table>
-			</div>
-		</div>
-		<div id="3879" class="vehicle row d-flex align-items-center">
-			<div class="vehicle-card col-1">
-				<div class="card text-center">
-					<span class="fa-solid fa-truck-medical"></span>
-				</div>
-				<div class="card-body">
-					<h5 class="card-title text-center">3879</h5>
-				</div>
-			</div>
-			<div class="crew-table-wrapper col-11">
-				<table class="table crew">
-					<?php
-						// iterate through JSON currentdata and populate crew data for 3879
-						$search_val_3879 = '1992';
-						if($json_data) {
-							foreach($json_data['currentdata'] as $currentdata) {
-								if ($currentdata['vehicle_id'] == $search_val_3879 && strtotime($currentdata['in']) < strtotime('+2 hour',time())) {
-									echo("<tr><td class=\"level ".$currentdata['level']."\">".$currentdata['level']."</td><td>".$currentdata['first_name']." ".$currentdata['last_name']."</td><td>".$currentdata['phone']."</td><td>".date('n/j  H:i', strtotime($currentdata['in']))."</td><td>".date('n/j  H:i', strtotime($currentdata['out']))."</td></tr>");
-								}
-							}
-						}
-					?>
-				</table>
-			</div>
-		</div>
-		<div id="MED36" class="vehicle row d-flex align-items-center" style="background: #efefef;">
-			<div class="vehicle-card col-1">
-				<div class="card text-center">
-					<span class="fa-solid fa-car-side"></span>
-				</div>
-				<div class="card-body">
-					<h5 class="card-title text-center">MED36</h5>
-				</div>
-			</div>
-			<div class="crew-table-wrapper col-11">
-				<table class="table crew">
-					<?php
-						// iterate through JSON currentdata and populate crew data for MED36
-						$search_val_36 = '1551';
-						if($json_data) {
-							foreach($json_data['currentdata'] as $currentdata) {
-								if ($currentdata['vehicle_id'] == $search_val_36 && strtotime($currentdata['in']) < strtotime('+2 hour',time())) {
-									echo("<tr><td class=\" level".$currentdata['level']."\">".$currentdata['level']."</td><td>".$currentdata['first_name']." ".$currentdata['last_name']."</td><td>".$currentdata['phone']."</td><td>".date('n/j  H:i', strtotime($currentdata['in']))."</td><td>".date('n/j  H:i', strtotime($currentdata['out']))."</td></tr>");
-								}
-							}
-						}
-					?>
-				</table>
-			</div>
-		</div>
-		<div id="MED38" class="vehicle row d-flex align-items-center">
-			<div class="vehicle-card col-1">
-				<div class="card text-center">
-					<span class="fa-solid fa-car-side"></span>
-				</div>
-				<div class="card-body">
-					<h5 class="card-title text-center">MED38</h5>
-				</div>
-			</div>
-			<div class="crew-table-wrapper col-11">
-				<table class="table crew">
-					<?php
-						// iterate through JSON currentdata and populate crew data for MED38
-						$search_val_38 = '1552';
-						if($json_data) {
-							foreach($json_data['currentdata'] as $currentdata) {
-								if ($currentdata['vehicle_id'] == $search_val_38 && strtotime($currentdata['in']) < strtotime('+2 hour',time())) {
-									echo("<tr><td class=\" level ".$currentdata['level']."\">".$currentdata['level']."</td><td>".$currentdata['first_name']." ".$currentdata['last_name']."</td><td>".$currentdata['phone']."</td><td>".date('n/j  H:i', strtotime($currentdata['in']))."</td><td>".date('n/j  H:i', strtotime($currentdata['out']))."</td></tr>");
-								}
-							}
-						}
-					?>
-				</table>
-			</div>
-		</div>
-	</div>
+				</div> <!-- end .bucket -->
+			</div> <!-- end .col -->
+		</div> <!-- end .row -->
+	</div> <!-- end .supervisors -->
+	<div class="container-fluid">
+		<div class="row">
+			<div id="duty-crew-wrapper" class="col-9">
+				<h2 class="duty-crew-heading">On-Duty Crews</h2>
+				<div class="vehicles container">
+					<div id="3859" class="vehicle row d-flex align-items-center">
+						<div class="vehicle-card col-1">
+							<div class="card text-center">
+								<span class="fa-solid fa-truck-medical"></span>
+							</div> <!-- end .card -->
+							<div class="card-body">
+								<h5 class="card-title text-center">3859</h5>
+							</div> <!-- end .card-body -->
+						</div> <!-- end vehicle-card -->
+						<div class="crew-table-wrapper col-11">
+							<table class="table crew">
+								<?php
+									// iterate through JSON currentdata and populate crew data for 3859
+									$search_val_3859 = '1990';
+									if($json_data) {
+										foreach($json_data['currentdata'] as $currentdata) {
+											if ($currentdata['vehicle_id'] == $search_val_3859 && strtotime($currentdata['in']) < strtotime('+2 hour',time())) {
+												echo("<tr><td class=\"level ".$currentdata['level']."\">".$currentdata['level']."</td><td>".$currentdata['first_name']." ".$currentdata['last_name']."</td><td>".$currentdata['phone']."</td><td>".date('n/j  H:i', strtotime($currentdata['in']))."</td><td>".date('n/j  H:i', strtotime($currentdata['out']))."</td></tr>");
+											}
+										}
+									}
+								?>
+							</table>
+						</div> <!-- end crew-table-wrapper -->
+					</div> <!-- end #3859 -->
+					<div id="3869" class="vehicle row d-flex align-items-center" style="background: #efefef;">
+						<div class="vehicle-card col-1">
+							<div class="card text-center">
+								<span class="fa-solid fa-truck-medical"></span>
+							</div> <!-- end card -->
+							<div class="card-body">
+								<h5 class="card-title text-center">3869</h5>
+							</div> <!-- end card-body -->
+						</div> <!-- end .vehicle-card -->
+						<div class="crew-table-wrapper col-11">
+							<table class="table crew">
+								<?php
+									// iterate through JSON currentdata and populate crew data for 3869
+									$search_val_3869 = '1991';
+									if($json_data) {
+										foreach($json_data['currentdata'] as $currentdata) {
+											if ($currentdata['vehicle_id'] == $search_val_3869 && strtotime($currentdata['in']) < strtotime('+2 hour',time())) {
+												echo("<tr><td class=\"level ".$currentdata['level']."\">".$currentdata['level']."</td><td>".$currentdata['first_name']." ".$currentdata['last_name']."</td><td>".$currentdata['phone']."</td><td>".date('n/j  H:i', strtotime($currentdata['in']))."</td><td>".date('n/j  H:i', strtotime($currentdata['out']))."</td></tr>");
+											}
+										}
+									}
+								?>
+							</table>
+						</div> <!-- end .crew-table-wrapper -->
+					</div> <!-- end #3869 -->
+					<div id="3879" class="vehicle row d-flex align-items-center">
+						<div class="vehicle-card col-1">
+							<div class="card text-center">
+								<span class="fa-solid fa-truck-medical"></span>
+							</div> <!-- end .card -->
+							<div class="card-body">
+								<h5 class="card-title text-center">3879</h5>
+							</div> <!-- end .card-body -->
+						</div>
+						<div class="crew-table-wrapper col-11">
+							<table class="table crew">
+								<?php
+									// iterate through JSON currentdata and populate crew data for 3879
+									$search_val_3879 = '1992';
+									if($json_data) {
+										foreach($json_data['currentdata'] as $currentdata) {
+											if ($currentdata['vehicle_id'] == $search_val_3879 && strtotime($currentdata['in']) < strtotime('+2 hour',time())) {
+												echo("<tr><td class=\"level ".$currentdata['level']."\">".$currentdata['level']."</td><td>".$currentdata['first_name']." ".$currentdata['last_name']."</td><td>".$currentdata['phone']."</td><td>".date('n/j  H:i', strtotime($currentdata['in']))."</td><td>".date('n/j  H:i', strtotime($currentdata['out']))."</td></tr>");
+											}
+										}
+									}
+								?>
+							</table>
+						</div> <!-- end .crew-table-wrapper -->
+					</div> <!-- end #3879 -->
+					<div id="MED36" class="vehicle row d-flex align-items-center" style="background: #efefef;">
+						<div class="vehicle-card col-1">
+							<div class="card text-center">
+								<span class="fa-solid fa-car-side"></span>
+							</div> <!-- end .vehicle-card -->
+							<div class="card-body">
+								<h5 class="card-title text-center">MED36</h5>
+							</div> <!-- end .card-body -->
+						</div>
+						<div class="crew-table-wrapper col-11">
+							<table class="table crew">
+								<?php
+									// iterate through JSON currentdata and populate crew data for MED36
+									$search_val_36 = '1551';
+									if($json_data) {
+										foreach($json_data['currentdata'] as $currentdata) {
+											if ($currentdata['vehicle_id'] == $search_val_36 && strtotime($currentdata['in']) < strtotime('+2 hour',time())) {
+												echo("<tr><td class=\" level".$currentdata['level']."\">".$currentdata['level']."</td><td>".$currentdata['first_name']." ".$currentdata['last_name']."</td><td>".$currentdata['phone']."</td><td>".date('n/j  H:i', strtotime($currentdata['in']))."</td><td>".date('n/j  H:i', strtotime($currentdata['out']))."</td></tr>");
+											}
+										}
+									}
+								?>
+							</table>
+						</div> <!-- end .crew-table-wrapper -->
+					</div> <!-- end #MED36 -->
+					<div id="MED38" class="vehicle row d-flex align-items-center">
+						<div class="vehicle-card col-1">
+							<div class="card text-center">
+								<span class="fa-solid fa-car-side"></span>
+							</div> <!-- end .vehicle-card -->
+							<div class="card-body">
+								<h5 class="card-title text-center">MED38</h5>
+							</div> <!-- end .card-body -->
+						</div>
+						<div class="crew-table-wrapper col-11">
+							<table class="table crew">
+								<?php
+									// iterate through JSON currentdata and populate crew data for MED38
+									$search_val_38 = '1552';
+									if($json_data) {
+										foreach($json_data['currentdata'] as $currentdata) {
+											if ($currentdata['vehicle_id'] == $search_val_38 && strtotime($currentdata['in']) < strtotime('+2 hour',time())) {
+												echo("<tr><td class=\" level ".$currentdata['level']."\">".$currentdata['level']."</td><td>".$currentdata['first_name']." ".$currentdata['last_name']."</td><td>".$currentdata['phone']."</td><td>".date('n/j  H:i', strtotime($currentdata['in']))."</td><td>".date('n/j  H:i', strtotime($currentdata['out']))."</td></tr>");
+											}
+										}
+									}
+								?>
+							</table>
+						</div> <!-- end crew-table-wrapper -->
+					</div> <!-- end #MED38 -->
+				</div> <!-- end .vehicles .container -->
+			</div> <!-- end #duty-crew-wrapper -->
+			<div id="sidebar" class="col-3">
+				<div id="weather" class="container">
+					<a class="weatherwidget-io" href="https://forecast7.com/en/43d09n77d51/pittsford/?unit=us" data-label_1="PITTSFORD" data-label_2="WEATHER" data-font="Roboto" data-icons="Climacons Animated" data-days="3" data-theme="pure" >PITTSFORD WEATHER</a>
+					<script>
+					!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+					</script>
+				</div> <!-- end #weather -->
+			</div> <!-- end #sidebar -->
+		</div> <!-- end .row -->
+	</div> <!-- end .container-fluid -->
 
     <!-- Optional JavaScript; choose one of the two! -->
 
